e3f6eb828563954031bc9eb2ad495fd7a10024c7
NghiaDinh003 <92510310055@ispace.edu.vn>
2022-02-27 12:43:19 +0700

    first commit

---
+++ 8_3 Noc Nha/config.js
@@ -0,0 +1,13 @@
+const nameGirl = '';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100008990858631&sk=photos_by';
+const eventName = 'Chúc Mừng 08/03/2022 Nóc Nhà';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc nóc nhà của Anh một ngày lễ thật hạnh phúc, thật vui khi có Chị kề bên!!!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Nga.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
